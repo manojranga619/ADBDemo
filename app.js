3df1e963f640511677ae16f84071fa859c0f608c
manojranga619 <84231573+manojranga619@users.noreply.github.com>
2022-02-13 17:50:23 -0600

    t

--- app.js
+++ app.js
@@ -87,40 +87,40 @@ app.post('/updateSalary', (req, res) => {
     }
 });
 
-app.post('/searchnamekeydb', (req, res) => {
-    var name = req.body.Name;
-    var value = req.body.Value;
-    var sql = `SELECT TOP 20 * FROM [details]`;
-    // Read all rows from table
-    const request = new Request(sql,
-        (err, rowCount) => {
-        if (err) {
-            console.error(err.message);
-        } else {
-            console.log(`${rowCount} row(s) returned`);
-            }
-        }
-    );
-    const rows = [];
-    request.on("row", columns => {
-        let row = {};
-        columns.forEach((column) => {
-            row[column.metadata.colName] = column.value;
-        });
-        rows.push(row);
-    });
+// app.post('/searchnamekeydb', (req, res) => {
+//     var name = req.body.Name;
+//     var value = req.body.Value;
+//     var sql = `SELECT TOP 20 * FROM [details]`;
+//     // Read all rows from table
+//     const request = new Request(sql,
+//         (err, rowCount) => {
+//         if (err) {
+//             console.error(err.message);
+//         } else {
+//             console.log(`${rowCount} row(s) returned`);
+//             }
+//         }
+//     );
+//     const rows = [];
+//     request.on("row", columns => {
+//         let row = {};
+//         columns.forEach((column) => {
+//             row[column.metadata.colName] = column.value;
+//         });
+//         rows.push(row);
+//     });
 
-    request.on('done', (rowCount) => {
-        console.log('Done is called!');
-        res.send(rows);
-      });
+//     request.on('done', (rowCount) => {
+//         console.log('Done is called!');
+//         res.send(rows);
+//       });
     
-    request.on('doneInProc', (rowCount, more) => {
-        console.log(rowCount + ' rows returned');
-        res.send(rows);
-    });
-   connection.execSql(request);
-});
+//     request.on('doneInProc', (rowCount, more) => {
+//         console.log(rowCount + ' rows returned');
+//         res.send(rows);
+//     });
+//    connection.execSql(request);
+// });
 
 const PORT =3000;
 app.listen(PORT, () => {
